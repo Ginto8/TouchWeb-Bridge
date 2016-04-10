4216d448906136a6893d8a1b596083f38376a46f
Joe Doyle <jpdoyle@andrew.cmu.edu>
2016-04-09 20:36:09 -0400

    Flask web service

--- foo.py
+++ foo.py
@@ -1,7 +1,13 @@
+from flask import Flask
 import lumiversepython as L
 import time
 import math
+import multiprocessing,Queue
 from threading import Thread,Lock
+
+DEBUG=True
+USE_DEBUGGER=True
+app=Flask(__name__)
 
 rig = L.Rig("/home/teacher/Lumiverse/PBridge.rig.json")
 
@@ -31,10 +37,6 @@ def updateLights():
         rig.updateOnce()
         time.sleep(0.025)
 
-bg_thread = Thread(target=updateLights)
-bg_thread.setDaemon(True)
-bg_thread.start()
-
 """
     use weight parameter (last (optional) parameter in setColorRGBRaw) to fade in colors...
     use the blank template to disable pharos 
@@ -61,15 +63,14 @@ def fade(factor=0.97):
         colors[ix] = (factor*r,factor*g,factor*b)
 
 def wave():
-    while(1):
-        for i in xrange(len(panels)):
-            with colorLock:
-                fade()
-                setColor(wrap(len(panels),i-2),(1,0,0))
-                setColor(wrap(len(panels),i-1),(0,1,0))
-                setColor(i,(0,0,1))
-            time.sleep(0.05)
-            # rig.updateOnce()
+    for i in xrange(2*len(panels)):
+        i = i%len(panels)
+        with colorLock:
+            fade()
+            setColor(wrap(len(panels),i-2),(1,0,0))
+            setColor(wrap(len(panels),i-1),(0,1,0))
+            setColor(i,(0,0,1))
+        time.sleep(0.05)
 
 def colorAdd(a,b):
     return (a[0]+b[0],a[1]+b[1],a[2]+b[2])
@@ -78,72 +79,56 @@ def colorMult(x,c):
     return (x*c[0],x*c[1],x*c[2])
 
 def collider():
-    while(1):
-        ax = 0
-        bx = len(panels)-1
-        v = 0.001
-        while(ax <= len(panels)-1 and bx >= 0):
-            ax += v/2
-            bx -= v/2
-            if ax <= bx:
-                v *= 1.085
+    ax = 0
+    bx = len(panels)-1
+    v = 0.001
+    while(ax <= len(panels)-1 and bx >= 0):
+        ax += v/2
+        bx -= v/2
+        if ax <= bx:
+            v *= 1.085
+        else:
+            v = max(0.94*v,0.2)
+
+        acolor = (1,1,1) if ax <= bx else (1,0,0)
+        bcolor = (1,1,1) if ax <= bx else (0.8,156/255.0,0)
+
+        with colorLock:
+            if ax < bx:
+                fade(0.8)
             else:
-                v = max(0.94*v,0.2)
+                fade(0.985)
+            lowBx = max(int(math.floor(bx)),0)
+            highAx = min(int(math.ceil(ax)),len(panels)-1)
+            frac = bx - lowBx
+            colors[highAx] = acolor
+            colors[lowBx] = bcolor
+            if highAx >= 1:
+                ix = highAx-1
+                colors[ix] = colorAdd(colorMult(frac,acolor),
+                                        colorMult((1-frac),colors[ix]))
+            if lowBx <= len(panels)-1:
+                ix = lowBx+1
+                colors[ix] = colorAdd(colorMult(frac,bcolor),
+                                        colorMult((1-frac),colors[ix]))
 
-            acolor = (1,1,1) if ax <= bx else (1,0,0)
-            bcolor = (1,1,1) if ax <= bx else (0.8,156/255.0,0)
+        time.sleep(0.025)
+    while sum((c[0] + c[1] + c[2])/3.0 for c in
+            colors)/float(len(panels)) > 0.01:
+        with colorLock:
+            fade(0.98)
+        time.sleep(0.025)
+    off()
 
-            with colorLock:
-                if ax < bx:
-                    fade(0.8)
-                else:
-                    fade(0.985)
-                lowBx = max(int(math.floor(bx)),0)
-                highAx = min(int(math.ceil(ax)),len(panels)-1)
-                frac = bx - lowBx
-                colors[highAx] = acolor
-                colors[lowBx] = bcolor
-                if highAx >= 1:
-                    ix = highAx-1
-                    colors[ix] = colorAdd(colorMult(frac,acolor),
-                                          colorMult((1-frac),colors[ix]))
-                if lowBx <= len(panels)-1:
-                    ix = lowBx+1
-                    colors[ix] = colorAdd(colorMult(frac,bcolor),
-                                          colorMult((1-frac),colors[ix]))
+def runRgb():
+    flash((1,0,0))
+    time.sleep(1)
+    flash((0,1,0))
+    time.sleep(1)
+    flash((0,0,1))
+    time.sleep(1)
+    off()
 
-            time.sleep(0.025)
-        while sum((c[0] + c[1] + c[2])/3.0 for c in
-                colors)/float(len(panels)) > 0.01:
-            with colorLock:
-                fade(0.98)
-            time.sleep(0.025)
-
-def run():
-    # flash((1,0,0))
-    # time.sleep(1)
-    # flash((0,1,0))
-    # time.sleep(1)
-    # flash((0,0,1))
-    # time.sleep(1)
-    # off()
-    # time.sleep(3)
-    # wave()
-    collider()
-'''
-	flashG()
-	time.sleep(1)
-	flashR()
-	time.sleep(1)
-        flashG()
-        time.sleep(1)
-	off()
-	time.sleep(1)
-	wave()
-	time.sleep(1)
-	off()
-	time.sleep(1)
-'''
 def step():
 	for x in xrange(100):
 		rig.select("panel=%d"%x).setColorRGBRaw("color",1,1,1)
@@ -151,6 +136,39 @@ def step():
 		rig.select("panel=%d"%x).setColorRGBRaw("color",0,0,0)
 		raw_input("Move on to next one?")
 
-while(1):
-	run()
+effectQueue = multiprocessing.Queue(5)
+
+@app.route('/runeffect/<effect>')
+def sendEffect(effect):
+    effectQueue.put(effect)
+    return ""
+
+def runFlask():
+    app.run(host='0.0.0.0')
+
+multiprocessing.Process(target=runFlask).start()
+
+bg_thread = Thread(target=updateLights)
+bg_thread.setDaemon(True)
+bg_thread.start()
+
+effectRunning = Lock()
+
+def runEffect(effect):
+    with effectRunning:
+        print "Running " + str(effect)
+        {
+            'rgb':runRgb,
+            'wave':wave,
+            'collide':collider
+        }[effect]()
+        print "Done"
+
+while True:
+    try:
+        x = effectQueue.get_nowait()
+        Thread(target=runEffect,args=[x]).start()
+    except Queue.Empty:
+        pass
+    time.sleep(0.1)
 
