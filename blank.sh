c3939f91e3538da459d63a1f25b01c9076022736
Joe Doyle <jpdoyle@andrew.cmu.edu>
2016-05-14 06:08:58 -0400

    New server IP

--- blank.sh
+++ blank.sh
@@ -1,1 +1,1 @@
-curl -F '~project_file=@/home/teacher/Pharos_Files/blank.006145.wrn' http://192.168.1.8/upload
+curl -F '~project_file=@/home/teacher/Pharos_Files/blank.006145.wrn' http://192.168.1.9/upload
--- default.sh
+++ default.sh
@@ -1,2 +1,2 @@
-curl -F '~project_file=@/home/teacher/Pharos_Files/Pausch_V27.006145.wrn' http://192.168.1.8/upload
-curl "http://192.168.1.8/trigger/4?conditions=0"
+curl -F '~project_file=@/home/teacher/Pharos_Files/Pausch_V27.006145.wrn' http://192.168.1.9/upload
+curl "http://192.168.1.9/trigger/4?conditions=0"
